dffc64566e7c1753e4b433fbfd0b81d46e292fa4
GavinLin512 <81368692+GavinLin512@users.noreply.github.com>
2025-02-19 18:20:43 +0800

    add dashboard, modify user table, update prisma

--- src/app/(admin)/admin/layout.tsx
+++ src/app/(admin)/admin/layout.tsx
@@ -1,6 +1,6 @@
 import type { Metadata } from "next";
 import { Geist, Geist_Mono } from "next/font/google";
-import "../../app/globals.css";
+import "../../../app/globals.css";
 import { ThemeProvider } from "@/components/theme-provider";
 
 const geistSans = Geist({
