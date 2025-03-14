14f776b9473abb0733e613c3051aa9e522fcd3d4
GavinLin512 <81368692+GavinLin512@users.noreply.github.com>
2025-03-14 08:24:08 +0800

    add nav to index

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,21 +1,27 @@
 import prisma from '@/lib/prisma'
 import type { User } from '@prisma/client'
+import Navigation from '@/components/navigation'
+
 
 const users = await prisma.user.findMany();
 
+
 export default async function Home() {
   return (
-    <div className="min-h-screen bg-background flex flex-col items-center justify-center -mt-16">
-      <h1 className="text-4xl font-bold mb-8 font-[family-name:var(--font-geist-sans)] text-[#333333]">
-        Superblog
-      </h1>
-      <ol className="list-decimal list-inside font-[family-name:var(--font-geist-sans)]">
-        {users.map((user: User) => (
-          <li key={user.id} className="mb-2">
-            {user.name} {user.email}
-          </li>
-        ))}
-      </ol>
-    </div>
+    <>
+      <Navigation />
+      <div className="min-h-screen bg-background flex flex-col items-center justify-center -mt-16">
+        <h1 className="text-4xl font-bold mb-8 font-[family-name:var(--font-geist-sans)] text-[#333333]">
+          Superblog
+        </h1>
+        <ol className="list-decimal list-inside font-[family-name:var(--font-geist-sans)]">
+          {users.map((user: User) => (
+            <li key={user.id} className="mb-2">
+              {user.name} {user.email}
+            </li>
+          ))}
+        </ol>
+      </div>
+    </>
   );
 }
